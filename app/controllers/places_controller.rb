36d9ca51185da62bbb86b9e8230b8d99081d6145
Laura Reifsnyder <laura.reifsnyder@kellogg.northwestern.edu>
2023-05-16 15:03:16 +0000

    commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,27 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = ["United States", "Mexico", "Morocco"]
+    @places = Place.all
+  end
+
+  def show
+    @place = Place.find_by({ "id" => params["id"] })
+    @posts = Post.where({ "place_id" => @place["id"] })
   end
 
   def new
-    @place = Place.all
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new
+    @place["name"] = params["place"]["name"]
+    @place.save
+    redirect_to "/places"
   end
 
 
+  
 end
+
+#  
