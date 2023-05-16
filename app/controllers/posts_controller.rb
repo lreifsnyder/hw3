f991ef307a7f56cebecdde4502856417a4d2940b
Laura Reifsnyder <laura.reifsnyder@kellogg.northwestern.edu>
2023-05-16 22:30:13 +0000

    commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,21 @@
 class PostsController < ApplicationController
+
   def index
-    render :template => "posts/index"
+    @posts = Post.all
+    @places = Place.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post["title"] = params["post"]["title"]
+    @post["date_created"] = params["post"]["date_created"]
+    @post["description"] = params["post"]["description"]
+    @post.save
+    redirect_to "/posts"
+  end
+
 end
